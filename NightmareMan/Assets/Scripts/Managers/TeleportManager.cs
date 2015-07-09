08fc7dab3a7a0b7bda77ab247c1355423a1a82bc
Krassen22 <krassen22@gmail.com>
2015-07-09 15:15:56 +0300

    Teleport added

---
+++ NightmareMan/Assets/Scripts/Managers/TeleportManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TeleportManager : MonoBehaviour {
+	public Transform otherTeleportPoint;
+
+	Rigidbody NightmareManRigid;
+	bool NightmareManInRange = false;
+
+	void OnTriggerEnter (Collider other)
+	{
+		if(other.gameObject.tag == "Player" && !NightmareManInRange)
+		{
+			NightmareManRigid = other.gameObject.GetComponent<Rigidbody> ();
+			NightmareManInRange = true;
+			Teleport();
+		}
+	}
+
+	void OnTriggerExit (Collider other)
+	{
+		if(other.gameObject.tag == "Player")
+		{
+			NightmareManInRange = false;
+		}
+	}
+
+
+	void Teleport() 
+	{
+		NightmareManRigid.transform.position = otherTeleportPoint.position;
+	}
+}
