7a10049fa0f08d0f751e09641841163ce914a241
Krassen22 <krassen22@gmail.com>
2015-07-10 14:03:49 +0300

    'PlayerDead' added

---
+++ NightmareMan/Assets/Scripts/Ghost/GhostHomicide.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GhostHomicide : MonoBehaviour {
+	
+	GameObject nightmareMan;
+	NightmareManMovement nightmareManMovement;
+	Animator nightmareManAnim;
+	CapsuleCollider nightmareManCapsule;
+
+	Animator GhostAnim;
+
+	void Awake()
+	{
+		GhostAnim = GetComponent<Animator> ();
+	}
+	
+	void OnTriggerEnter (Collider other)
+	{
+		if(other.gameObject.tag == "Player")
+		{
+			nightmareMan = other.gameObject;
+
+			nightmareManMovement = nightmareMan.GetComponent<NightmareManMovement> ();
+			nightmareManAnim = nightmareMan.GetComponent<Animator> ();
+			nightmareManCapsule = nightmareMan.GetComponent<CapsuleCollider> ();
+			nightmareManMovement.enabled = false;
+			nightmareManCapsule.enabled = false;
+
+			nightmareMan.tag = "PlayerDead";
+			nightmareManAnim.SetTrigger ("Die");
+			GhostAnim.SetTrigger("NightmareManWinOrDie");
+		}
+	}
+}
--- NightmareMan/Assets/Scripts/Managers/GameStateManager.cs
+++ NightmareMan/Assets/Scripts/Managers/GameStateManager.cs
@@ -4,6 +4,19 @@ using System.Collections;
 public class GameStateManager : SingletonManager<GameStateManager> {
 
 	public enum States { Pause, Start, Resume, Win, Lose }
-	public States gameState;
+	// public States gameState;
 	
+
+	void Update()
+	{
+	
+	}
+
+	public bool IsGameOver() {
+		return GameObject.FindGameObjectWithTag ("Player") == null;
+	}
+
+	// public void PlayAgain(){
+	// 	Application.LoadLevel ("Level 01");
+	// }
 }
