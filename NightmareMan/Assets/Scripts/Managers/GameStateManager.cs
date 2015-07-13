3fb2fccbd8620ee24d9d94dc3577cd2cf3a6825a
Krassen22 <krassen22@gmail.com>
2015-07-13 13:06:13 +0300

    Win and Game Over animations

--- NightmareMan/Assets/Scripts/Managers/GameStateManager.cs
+++ NightmareMan/Assets/Scripts/Managers/GameStateManager.cs
@@ -5,10 +5,21 @@ public class GameStateManager : SingletonManager<GameStateManager> {
 
 	public enum States { Pause, Start, Resume, Win, Lose }
 	public States gameState;
+	Animator anim;
 
 	new void Awake() {
 		base.Awake ();
 		InitGameData ();
+		anim = GetComponent<Animator> ();
+	}
+
+	void Update(){
+		if(gameState == States.Lose) {
+			anim.SetTrigger("GameOver");
+		}
+		if(gameState == States.Win) {
+			anim.SetTrigger("YouWin");
+		}
 	}
 
 	void InitGameData() {
