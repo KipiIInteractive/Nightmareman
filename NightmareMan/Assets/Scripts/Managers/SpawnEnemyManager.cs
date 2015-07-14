37064812200269bf7d7c5665c8c0cede9536d41c
Krassen22 <krassen22@gmail.com>
2015-07-14 16:34:57 +0300

    Small changes

--- NightmareMan/Assets/Scripts/Managers/SpawnEnemyManager.cs
+++ NightmareMan/Assets/Scripts/Managers/SpawnEnemyManager.cs
@@ -1,27 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class SpawnEnemyManager : SingletonManager<SpawnEnemyManager> {
 
-	public GameObject enemy;
-	public int numberOfZombies 		= 4;
+	public List<GameObject> enemy;
 	public int spawnIntervalSeconds = 4;
-
+	static int count = 0;
 	Transform spawnPoint;
 
 	new void Awake() {
 		base.Awake ();
 		spawnPoint = GameObject.FindGameObjectWithTag ("SpawnEnemyPoint").transform;
-		for(int i = 0; i < numberOfZombies; i++)
-			Invoke("SpawnEnemy", spawnIntervalSeconds*i);
+		for(int i = 0; i < enemy.Count; i++)
+			Invoke( "SpawnEnemy", spawnIntervalSeconds*i);
 	}
 	
-	public void SpawnEnemy() {
-		Instantiate (enemy, spawnPoint.position, enemy.transform.rotation);
+	public void SpawnEnemy() { 
+		Instantiate (enemy[count], spawnPoint.position, enemy[count].transform.rotation);
+		count++;
 	}
 
 	public void Respawn(GameObject enemy) {
 		enemy.transform.position = spawnPoint.position;
 	}
-
 }
